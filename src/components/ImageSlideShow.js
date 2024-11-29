e7635c2b946920869727212103bd22cc597d1013
Sainath <sainathdora4@gmail.com>
2024-11-29 22:38:19 +0530

    added slideshow

--- src/components/ImageSlideShow.js
+++ src/components/ImageSlideShow.js
@@ -1,8 +1,55 @@
-import Image from "next/image";
-export default function ImageSlideShow() {
+import { useEffect, useState } from "react";
+import classes from "./Image.module.css";
+const ImageSlideShow = () => {
+  const slides = [
+    {
+      id: "img1",
+      src: "/assets/img1.jpg",
+      alt: "Very fast car",
+    },
+    {
+      id: "img2",
+      src: "/assets/img2.jpg",
+      alt: "Very fast car 2",
+    },
+    {
+      id: "img3",
+      alt: "Very fast car 3",
+      src: "/assets/img3.jpg",
+    },
+  ];
+  const [currentindex, setindex] = useState(0);
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setindex((prev) => {
+        if (prev < slides.length - 1) {
+          return prev + 1;
+        } else {
+          return 0;
+        }
+      });
+    }, 5000);
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
   return (
     <>
-      <Image src="/assets/img1.jfif" alt="Food1" width={100} height={100} />
+      <div className={classes.imageslide}>
+        {slides.map((image, index) => {
+          return (
+            <img
+              src={image.src}
+              alt={image.alt}
+              className={index === currentindex ? classes.active : ""}
+              key={index}
+            />
+          );
+        })}
+      </div>
     </>
   );
-}
+};
+
+export default ImageSlideShow;
--- src/pages/index.js
+++ src/pages/index.js
@@ -22,7 +22,55 @@ export default function Home() {
       <div className="flex flex-col min-h-screen">
         <main className="flex-grow">
           <NavBar></NavBar>
-          <ImageSlideShow />
+          <header className="grid grid-cols-2 gap-4">
+            <ImageSlideShow />
+            <section>
+              <h1 className="text-center text-4xl font-bold">Obesity</h1>
+              <p className="p-4">
+                In our effort to combat the rising health challenges in India,
+                we’ve developed a cutting-edge deep learning model fine-tuned on
+                a diverse Indian food dataset. This model, powered by the
+                EfficientNetB7 architecture, is designed to predict the
+                macronutrients (carbohydrates, proteins, fats, and calories) in
+                food based on images uploaded by the user. With obesity and
+                related health issues becoming increasingly prevalent in India,
+                our tool aims to provide individuals with valuable insights into
+                the nutritional content of the food they consume. By empowering
+                users with this knowledge, we hope to inspire healthier eating
+                habits and contribute to the ongoing efforts to tackle obesity,
+                diabetes, and heart diseases that are affecting millions of
+                people in the country. Our goal is to promote awareness, improve
+                health outcomes, and encourage informed food choices, ultimately
+                creating a healthier future for India.
+              </p>
+            </section>
+          </header>
+          <section>
+            <h1 className="text-center text-7xl">
+              Fine Tuned On EfficientNetB7
+            </h1>
+            <p className="text-center text-lg">
+              EfficientNetB7, a state-of-the-art deep learning model, boasts up
+              to 60 million parameters, making it one of the most powerful
+              architectures for image recognition tasks. This model has been
+              fine-tuned on our custom Indian food cuisine dataset, which
+              includes thousands of high-quality images representing a wide
+              variety of traditional Indian dishes.
+            </p>
+          </section>
+          <section>
+            <h1 className="text-center text-6xl font-bold text-green-600">
+              Achieving 80% Accuracy on Test Data
+            </h1>
+            <p className="text-center text-lg mt-4">
+              Our deep learning model, fine-tuned on a custom Indian food
+              dataset, has successfully achieved an impressive 80% accuracy on
+              test data. This milestone demonstrates the model&apos;s ability to
+              make accurate predictions for macronutrient content, offering
+              users a reliable tool for understanding the nutritional value of
+              their meals.
+            </p>
+          </section>
           <Form />
         </main>
         <Footer />
